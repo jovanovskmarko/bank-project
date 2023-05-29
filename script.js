3c5e40f4991811975a4b5296f636a22d3b689bf4
jovanovskmarko <marko.jovanovski66@gmail.com>
2023-05-29 17:28:55 +0200

    display movements

--- script.js
+++ script.js
@@ -75,5 +75,19 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let newArray = [1,2,3,...movements]
-console.log(newArray)
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function(mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
+    const html = 
+                `<div class="movements__row">
+                  <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+                  <div class="movements__value">${mov}</div>
+                </div>`
+    containerMovements.insertAdjacentHTML('afterbegin',html);
+
+  })
+}
+
+displayMovements(account1.movements);
